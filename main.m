36286de369957075fccf22f2b8f7fd51b478fb55
pavanj16 <pavanj@umich.edu>
2024-12-15 12:11:03 -0500

    minor edits

--- main.m
+++ main.m
@@ -35,7 +35,7 @@ trunc_length_HY = 20; % m, the length we truncate off the engine nozzle
 % --- Hydrocarbon(LCH4) Engine
 HC_Chamber_Pres = 160 * 1e5; % Pa
 HC_throat_area = 0.2; % m2, throat area
-R_c_HC = 0.2; % m, engine chamber radius
+R_c_HC = 0.28; % m, engine chamber radius
 L_c_HC = 1.04; % m, engine chamber axial length
 trunc_length_HC = 3; % m, the length we truncate off the engine nozzle
 
@@ -377,12 +377,17 @@ A_chamber = Ac_At * A_throat; % Calculate chamber cross-sectional area
 L_star = 80; % Characteristic chamber length in cm for LOX/CH4
 L_chamber = (L_star / 100) * A_chamber / A_throat; % Actual chamber length in meters
 
+% To claculate the combustion chamber and throat diameter
+Cc_dia = sqrt(A_chamber/3.14);
+Throat_dia = sqrt(A_throat/3.14);
+
 % Display chamber design results
 fprintf('Updated Characteristic chamber length (L*): %.2f cm\n', L_star);
 fprintf('Updated Chamber area: %.4f m^2\n', A_chamber);
 fprintf('Updated Chamber length: %.2f m\n', L_chamber);
 fprintf('Updated Chamber to throat area ratio (Ac/At): %.2f\n', Ac_At);
-fprintf('Pressure drop across injectors: %.2f Pa.\n', DeltaP);
+fprintf('Diameter of Combustion Chamber: %.2f m\n', Cc_dia);
+fprintf('Diameter of Throat: %.2f m\n', Throat_dia);
 
 % Add spacing between CH4 and H2 outputs for better console formatting
 fprintf('\n====================\n\n');
@@ -449,14 +454,16 @@ A_chamber = Ac_At * A_throat; % Calculate chamber cross-sectional area
 % Calculate characteristic chamber length (L*) and actual chamber length
 L_star = 56; % Characteristic chamber length in cm for LOX/H2
 L_chamber = (L_star / 100) * A_chamber / A_throat; % Actual chamber length in meters
+Cc_dia = sqrt(A_chamber/3.14); % in meters
+Throat_dia = sqrt(A_throat/3.14); % in meters
 
 % Display chamber design results
 fprintf('Updated Characteristic chamber length (L*): %.2f cm\n', L_star);
 fprintf('Updated Chamber area: %.4f m^2\n', A_chamber);
 fprintf('Updated Chamber length: %.2f m\n', L_chamber);
 fprintf('Updated Chamber to throat area ratio (Ac/At): %.2f\n', Ac_At);
-fprintf('Pressure drop across injectors: %.2f Pa.\n', DeltaP);
-
+fprintf('Diameter of Combustion Chamber: %.2f m\n', Cc_dia);
+fprintf('Diameter of Throat: %.2f m\n', Throat_dia);
 
 % --------------------Plotting---------------------%
 
